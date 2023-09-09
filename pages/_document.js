e1446ab8aae44db94a56a60fe3b4244f4dc9a210
Anastasia <llingveri@gmail.com>
2023-09-09 03:16:14 +0300

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -47,6 +47,11 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Ysabeau+SC:wght@1;100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" type="image/png" />
+        <link rel="android-chrome-192x192" href="/android-chrome-192x192.png" type="image/png" sizes='192x192' />
+        <link rel="android-chrome-512x512" href="/android-chrome-512x512.png" type="image/png" sizes='512x512' />
+        <link rel="icon" href="/favicon.ico" />
         {/* <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-DC5RRWLRNV"
