94ae74b5d823c0fc1889434ccde2070136d2b92b
Anastasia <llingveri@gmail.com>
2023-09-12 00:21:00 +0300

    marks

--- pages/_document.js
+++ pages/_document.js
@@ -3,43 +3,6 @@ import { ColorModeScript } from '@chakra-ui/react';
 import theme from '@deep-foundation/deepcase/imports/theme/theme';
 
 
-// const metadata: Metadata = {
-//   title: 'Deep Foundation',
-//   authors: [{ name: 'Deep Foundation' }, { name: 'Deep Foundation', url: 'https://deep.foundation' }],
-//   keywords: ['Deep Foundation', 'React', 'JavaScript'],
-//   applicationName: 'Deep Foundation',
-//   description: 'Deep Foundation',
-//   icons: {
-//     icon: 'image/png',
-//     shortcut: 'image/png',
-//     apple: '/apple-touch-icon.png',
-//     other: {
-//       rel: 'apple-touch-icon-precomposed',
-//       url: '/apple-touch-icon-precomposed.png',
-//     },
-//   },
-//   manifest: '/site.webmanifest',
-//   viewport: {
-//     width: 'device-width',
-//     initialScale: 1,
-//     minimumScale: 1,
-//   },
-//   category: 'technology',
-//   alternates: {
-//     canonical: '/',
-//     // languages: {
-//     //   'en-US': '/en-US',
-//     //   'de-DE': '/de-DE',
-//     // },
-
-//   },
-//   openGraph: {
-//     images: '/logo_n.svg',
-//   },
-
-// }
-
-
 export default function Document() {
   return (
     <Html>
@@ -55,7 +18,7 @@ export default function Document() {
         {/* <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-DC5RRWLRNV"
-        />
+        />*/}
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -65,7 +28,7 @@ export default function Document() {
               gtag('config', 'G-DC5RRWLRNV');
             `,
           }}
-        /> */}
+        /> 
         {/* <script src="https://gftruj.github.io/hand.tracking.controls.extras/dist/aframe-hand-tracking-controls-extras.js"></script>
         <script src="https://gftruj.github.io/hand.tracking.controls.extras/components/dist/hand-tracking-controls-extras-components.js"> </script> */}
       </Head>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,7 @@
-import React, { useEffect } from 'react';
-import {
-  Text,
-  Link,
-  Stack,
-  Card,
-  CardBody,
-  Heading,
-  CardHeader,
-} from '@chakra-ui/react';
-import {
-  DeepClient,
-} from '@deep-foundation/deeplinks/imports/client';
-import NextLink from 'next/link';
-
-
-import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import React from 'react';
 import { Page } from '../components/page';
 import { Provider } from '../imports/provider';
+
 
 export default function IndexPage() {
   return (
